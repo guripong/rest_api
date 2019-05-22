c8dcb5294ee8c861766ccc1b4327612d7be577bb
john.so <john.so@koreapolyschool.com>
2019-05-22 15:06:20 +0900

    1234

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,81 @@
 var express = require('express');
 var router = express.Router();
-
+const multiparty = require('multiparty');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req,res,next){
-  console.log('여기:',req.body);
-  
-  return res.json({'name':'hi'});
 
+
+router.post('/', function (req, res, next) {
+  console.log('post왔당');
+
+
+  var fname;
+  var form = new multiparty.Form({
+    autoFiles: false, // 요청이 들어오면 파일을 자동으로 저장할 것인가
+    uploadDir: 'temp/', // 파일이 저장되는 경로(프로젝트 내의 temp 폴더에 저장됩니다.)
+  });
+  var ct;
+  var realpath;
+
+  form.parse(req);
+  form.on('field', (name, value) => {
+    console.log('name',name);
+    console.log('value',value);
+    //console.log(`name:`,name,`value`,value);
+    if (name == 'fakepath') {
+      fakepath = value;
+    }
+  });
+  form.on('file', (name, file) => {
+    //console.log('파일 들어옴');
+    console.log(`파일:`, file.originalFilename);
+    fname = file.originalFilename;
+    realpath = file.path;
+    //console.log(file.headers['content-type']);
+    ct = file.headers['content-type'];
+
+  });
+
+  form.on('part', function (part) {
+    console.log('파트 들어옴');
+    //console.log(part);
+    var size;
+    if (part.filename) {
+      size = part.byteCount;
+      console.log(`size:`, size);
+
+    } else {
+      console.log('다시다시');
+      part.resume();
+    }
+
+  });
+
+  form.on('progress', function (byteRead, byteExpected) {
+    //받는도중 계속 호출
+    console.log(' Reading total  ' + byteRead + '/' + byteExpected);
+
+  });
+
+  form.on('error', function (err) {
+    console.log('Error parsing form: ' + err.stack);
+  });
+
+  form.on('close', () => {
+    console.log(`close!!!`);
+    //const fileName = './resources/ManualTest.wav';
+    console.log('fname:',fname);
+    console.log('realpath:',realpath);
+
+     return res.json({
+      status: true,
+      "fname": fname,
+      "realpath":realpath,
+    });
+   
+  });//form.on close
 });
 module.exports = router;
